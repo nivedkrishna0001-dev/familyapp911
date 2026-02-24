733fd5d617e0dc2a40c1bc53586936f70a1d8935
nivedkrishna0001-dev <nivedkrishna0001@gmail.com>
2026-02-24 22:25:04 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Simple pass-through to satisfy PWA install requirements
-  e.respondWith(fetch(e.request).catch(() => console.log('Network request failed')));
-});
